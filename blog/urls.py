fc8297e17099bee35ae66959f1bac0539e2b4005
Camron Khan <camron.khan@gmail.com>
2017-06-23 21:44:21 +0000

    changed article list offset from 3 to 10

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from django.views.generic import ListView, DetailView
 from blog.models import Article
 
 urlpatterns = [
-	url(r'^$', ListView.as_view(queryset=Article.objects.all().order_by('-go_live')[:3], template_name='blog/article_list.html')),
+	url(r'^$', ListView.as_view(queryset=Article.objects.all().order_by('-go_live')[:10], template_name='blog/article_list.html')),
 	url(r'^(?P<pk>\d+)$', DetailView.as_view(model=Article, template_name='blog/article.html'))
 ]
