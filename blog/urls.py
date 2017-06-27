b901600de7cf77c057d448c5340d217fc02307c7
Camron Khan <camron.khan@gmail.com>
2017-06-27 18:31:02 +0000

    implemented filtering articles by search bar and tag click

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from django.views.generic import ListView, DetailView
 from blog.models import Article
+from blog.views import find_articles_via_search, find_articles_via_tag
 
 urlpatterns = [
 	url(r'^$', ListView.as_view(queryset=Article.objects.all().order_by('-go_live')[:10], template_name='blog/article_list.html')),
-	url(r'^(?P<pk>\d+)$', DetailView.as_view(model=Article, template_name='blog/article.html'))
+	url(r'^(?P<pk>\d+)$', DetailView.as_view(model=Article, template_name='blog/article.html')),
+	url(r'^search/$', find_articles_via_search, name='search'),
+	url(r'^tag/$', find_articles_via_tag, name='tag')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-"""
-def index(request):
-	return HttpResponse("<h2>HEY!!!</h2>")
-"""
+from django.db.models import Q
+from blog.models import Article
+
+def find_articles_via_search(request):
+	queryset = Article.objects.all().order_by('-go_live')
+	query = request.GET.get('q')
+
+	if query:
+		queryset = queryset.filter(
+				Q(title__icontains=query) |
+				Q(summary__icontains=query)
+			).distinct()
+
+	context = { 'object_list': queryset }
+
+	return render(request, 'blog/article_list.html', context)
+
+
+def find_articles_via_tag(request):
+	queryset = Article.objects.all().order_by('-go_live')
+	query = request.GET.get('id')
+
+	if query:
+		queryset = queryset.filter(tags__id=query).distinct()
+
+	context = { 'object_list': queryset }
+
+	return render(request, 'blog/article_list.html', context)
+
