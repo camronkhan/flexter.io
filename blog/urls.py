dc8acaea3d3b66d3b8ffe951ccf17f49888ee327
Camron Khan <camron.khan@gmail.com>
2017-07-14 19:03:27 +0000

    added pagination

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from django.views.generic import ListView, DetailView
 from blog.models import Article
-from blog.views import find_articles_via_search, find_articles_via_tag
+from blog.views import find_articles_via_search, find_articles_via_tag, load_article_list
 
 urlpatterns = [
-	url(r'^$', ListView.as_view(queryset=Article.objects.all().order_by('-go_live')[:10], template_name='blog/article_list.html')),
+	#url(r'^$', ListView.as_view(queryset=Article.objects.all().order_by('-go_live')[:10], template_name='blog/article_list.html')),
+	url(r'^$', load_article_list, name = 'blog'),
 	url(r'^(?P<pk>\d+)$', DetailView.as_view(model=Article, template_name='blog/article.html')),
 	url(r'^search/$', find_articles_via_search, name='search'),
 	url(r'^tag/$', find_articles_via_tag, name='tag')
--- blog/views.py
+++ blog/views.py
@@ -1,31 +1,68 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import Q
 from blog.models import Article
 
+articles_per_page = 5
+
 def find_articles_via_search(request):
-	queryset = Article.objects.all().order_by('-go_live')
-	query = request.GET.get('q')
-
-	if query:
-		queryset = queryset.filter(
-				Q(title__icontains=query) |
-				Q(summary__icontains=query)
-			).distinct()
-
-	context = { 'object_list': queryset }
-
-	return render(request, 'blog/article_list.html', context)
+    # get article list
+	articles = Article.objects.all().order_by('-go_live')
+	# get 'q' query param from url
+	query_param = request.GET.get('q')
+	# filter via query param
+	if query_param:
+		articles = articles.filter(Q(title__icontains=query_param) | Q(summary__icontains=query_param)).distinct()
+		pagination_query_param = '&q=' + query_param
+	# get 'page' query param from url
+	page = request.GET.get('page', 1)
+	# get paginator with max num articles
+	paginator = Paginator(articles, articles_per_page)
+	# if no page exists, use page == 1
+	try:
+		articles = paginator.page(page)
+	except PageNotAnInteger:
+		articles = paginator.page(1)
+	except EmptyPage:
+		articles = paginator.page(paginator.num_pages)
+	# render result
+	return render(request, 'blog/article_list.html', { 'articles': articles, 'pagination_query_param': pagination_query_param })
 
 
 def find_articles_via_tag(request):
-	queryset = Article.objects.all().order_by('-go_live')
-	query = request.GET.get('id')
+	# get article list
+	articles = Article.objects.all().order_by('-go_live')
+	# get 'q' query param from url
+	query_param = request.GET.get('id')
+	# filter via query param
+	if query_param:
+		articles = articles.filter(tags__id=query_param).distinct()
+		pagination_query_param = '&id=' + query_param
+	# get 'page' query param from url
+	page = request.GET.get('page', 1)
+	# get paginator with max num articles
+	paginator = Paginator(articles, articles_per_page)
+	# if no page exists, use page == 1
+	try:
+		articles = paginator.page(page)
+	except PageNotAnInteger:
+		articles = paginator.page(1)
+	except EmptyPage:
+		articles = paginator.page(paginator.num_pages)
+	# render result
+	return render(request, 'blog/article_list.html', {'articles': articles, 'pagination_query_param': pagination_query_param })
 
-	if query:
-		queryset = queryset.filter(tags__id=query).distinct()
 
-	context = { 'object_list': queryset }
-
-	return render(request, 'blog/article_list.html', context)
+def load_article_list(request):
+	articles = Article.objects.all().order_by('-go_live')
+	page = request.GET.get('page', 1)
+	paginator = Paginator(articles, articles_per_page)
+	try:
+		articles = paginator.page(page)
+	except PageNotAnInteger:
+		articles = paginator.page(1)
+	except EmptyPage:
+		articles = paginator.page(paginator.num_ages)
+	return render(request, 'blog/article_list.html', { 'articles': articles })
 
